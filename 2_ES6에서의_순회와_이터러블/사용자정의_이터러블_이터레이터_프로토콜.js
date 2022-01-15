0c6eb51f93fc222f42fedb517b4998e73875ea85
JeonMinJae <ssi02014@naver.com>
2022-01-16 02:40:49 +0900

    Feat: 예제 추가

--- 2_ES6에서의_순회와_이터러블/사용자정의_이터러블_이터레이터_프로토콜.js
+++ 2_ES6에서의_순회와_이터러블/사용자정의_이터러블_이터레이터_프로토콜.js
@@ -1,12 +1,12 @@
 // 사용자 정의 이터러블 이터레이터 예제
 const iterable = {
   [Symbol.iterator]() {
-    let i = 3;
+    let i = 0;
     return {
       next() {
-        return i === 0
+        return i === 3
           ? { value: undefined, done: true }
-          : { value: i--, done: false };
+          : { value: i++, done: false };
       },
       [Symbol.iterator]() {
         return this;
@@ -17,10 +17,13 @@ const iterable = {
 
 // 이터레이터가 자기 자신을 반환하는 [Symbol.iterator]() 메서드를 갖고있으면 well formed 이터러블/이터레이터라고 한다.
 let iterator = iterable[Symbol.iterator]();
-iterator.next();
+console.log(iterator.next()); // { value: 0, done: false }
 
 console.log(iterator[Symbol.iterator]() === iterator); // true 자기 자신을 반환
 
 for (const el of iterator) {
-  console.log(el); // 2 1
+  console.log(el); // 1 2
 }
+
+const newIterator = [...iterable]; // [0, 1, 2]
+console.log(newIterator);
