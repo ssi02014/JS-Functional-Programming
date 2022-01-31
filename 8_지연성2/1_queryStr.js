203629b04b7fba54878c0c104dd4af6f4654baec
JeonMinJae <ssi02014@naver.com>
2022-01-31 21:30:33 +0900

    Add: L.entries

--- 8_지연성2/1_queryStr.js
+++ 8_지연성2/1_queryStr.js
@@ -1,9 +1,25 @@
 const { go, reduce, curry, map, pipe } = require("../lib");
+const L = require("../lib/delay");
+
+const join = curry((seperator = ",", iterator) => {
+  return reduce((a, b) => `${a}${seperator}${b}`, iterator);
+});
 
 const queryStr = pipe(
-  Object.entries,
-  map(([key, value]) => `${key}=${value}`),
-  reduce((acc, cur) => `${acc}&${cur}`)
+  L.entries,
+  L.map(([key, value]) => `${key}=${value}`),
+  join("&")
 );
 
+function* a() {
+  yield 10;
+  yield 11;
+  yield 12;
+  yield 13;
+  yield 14;
+  yield 15;
+}
+
 console.log(queryStr({ limit: 10, offset: 10, type: "notice" }));
+console.log(join("&", [1, 2, 3, 4]));
+console.log(join("&", a()));
