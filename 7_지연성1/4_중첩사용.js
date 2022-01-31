b08f6625f87c7d4a15381ae90c17bfcd453636a0
JeonMinJae <ssi02014@naver.com>
2022-01-31 20:19:49 +0900

    Add: map, filter 계열 함수의 결합 법칙

--- 7_지연성1/4_중첩사용.js
+++ 7_지연성1/4_중첩사용.js
@@ -16,3 +16,23 @@ go(
   take(2),
   console.log
 );
+
+go(
+  L.range(10),
+  L.filter((n) => n % 2),
+  L.map((n) => n + 10),
+  take(2),
+  console.log
+);
+
+/* 
+  map, filter 계열 함수들이 가지는 결합 법칙 
+
+  - 사용하는 데이터가 무엇인지
+  - 사용하는 보조 함수가 순수 함수라면 무엇이든지
+  - 아래와 같이 결합한다면 둘 다 결과가 같다.
+
+  [[mapping, mapping], [filtering, filtering], [mapping, mapping]]
+  =
+  [[mapping, filtering, mapping], [mapping, filtering, mapping]]
+*/
